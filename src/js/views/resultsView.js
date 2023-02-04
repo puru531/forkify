569690a39997778152ee4b2ebc1225043a7a54bd
puru531 <purushottam1619@gmail.com>
2023-02-05 02:04:59 +0530

    Initial commit

---
+++ src/js/views/resultsView.js
@@ -0,0 +1,41 @@
+//to show the seached results list
+import icons from 'url:../../img/icons.svg';
+import View from './view.js';
+import previewView from './previewView.js';
+class ResultsView extends View{
+    _parentEl = document.querySelector('.results');
+    _errorMessage = 'No recipes found for your query. Please try again!';
+    _message = '';
+
+    _generateMarkup() {
+        
+      return this._data.map(bookmark => previewView.render(bookmark, false)).join('');
+
+    }
+    /* OR
+    _generateMarkup() {
+        
+        return this._data.map(this._generateMarkupPreview).join('');
+
+    }
+    _generateMarkupPreview(data) {
+        //storing the current recipe being shown in detail --> if this id and list of recipes id is same then preview__link--active class will be added
+        const id = window.location.hash.slice(1);
+        return `
+            <li class="preview">
+            <a class="preview__link ${data.id === id ? 'preview__link--active' : ''}" href="#${data.id}">
+              <figure class="preview__fig">
+                <img src="${data.img}" alt="${data.title}" />
+              </figure>
+              <div class="preview__data">
+                <h4 class="preview__title">${data.title}</h4>
+                <p class="preview__publisher">${data.publisher}</p>
+              </div>
+            </a>
+          </li>
+          `;
+    }
+    */
+}
+
+export default new ResultsView();
